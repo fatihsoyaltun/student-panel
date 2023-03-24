52ff6bba7d427246c98efdfb31805877c2f13765
fatihsoyaltun <soyaltunfatih@gmail.com>
2023-03-24 17:46:09 +0300

    24Mart add

--- app.js
+++ app.js
@@ -25,3 +25,104 @@ submenus.forEach(submenu => {
 // activeBreadcrumbItem.innerHTML = 'Örnek Sayfa';
 
 
+//! Apex Charts
+
+var options = {
+  series: [{
+    name: "Eğitmen",
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+},
+{
+  name:"Öğrenci",
+  data:[20,34,45,56,67,78,89,90,78]
+}],
+  chart: {
+  height: 350,
+  type: 'line',
+  zoom: {
+    enabled: false
+  }
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+title: {
+  text: 'Product Trends by Month',
+  align: 'left'
+},
+grid: {
+  row: {
+    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+    opacity: 0.5
+  },
+},
+xaxis: {
+  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',],
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#apexcharts-area"), options);
+chart.render();
+
+//* Charts Two
+
+var options2 = {
+  series: [{
+    name: "Eğitmen",
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+},
+{
+  name:"Öğrenci",
+  data:[20,34,45,56,67,78,89,90,78]
+}],
+  chart: {
+  height: 350,
+  type: 'bar',
+  zoom: {
+    enabled: false
+  }
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+title: {
+  text: 'Product Trends by Month',
+  align: 'left'
+},
+grid: {
+  row: {
+    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+    opacity: 0.5
+  },
+},
+xaxis: {
+  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',],
+}
+};
+
+var chart2 = new ApexCharts(document.querySelector("#apexcharts-bar"), options2);
+chart2.render();
+
+
+//! Calendar
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var calendarEl = document.getElementById('calendar');
+//   var calendar = new FullCalendar.Calendar(calendarEl, {
+//     initialView: 'dayGridMonth',
+//     selectable: true,
+//     select: function(start, end ,allDays){
+//       console.log(start,end,allDays);
+//     }
+//   });
+//   calendar.render();
+// });
+// <div id="calendar" class="calendar-container"></div>
+
+
