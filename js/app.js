a2de0299761ac2a2ef5897cf13785ac665fa4940
Fatih SOYALTUN <soyaltunfatih@gmail.com>
2023-05-16 16:52:02 +0300

    menu open/close

--- js/app.js
+++ js/app.js
@@ -19,6 +19,41 @@ submenus.forEach(submenu => {
   });
 });
 
+//* geliştirilecek
+
+// const submenus = document.querySelectorAll(".submenu a");
+
+// submenus.forEach(submenu => {
+//   const subcontent = submenu.nextElementSibling;
+//   const arrow = submenu.querySelector(".menu-arrow");
+
+//   submenu.addEventListener("click", () => {
+//     // Diğer menülerin otomatik kapanması
+//     const openSubmenus = document.querySelectorAll(".submenu a.active");
+//     openSubmenus.forEach(openSubmenu => {
+//       const openSubcontent = openSubmenu.nextElementSibling;
+//       const openArrow = openSubmenu.querySelector(".menu-arrow");
+//       openSubcontent.style.display = "none";
+//       openArrow.style.transform = "rotate(0deg)";
+//       openSubmenu.classList.remove("active");
+//     });
+
+//     // Seçilen menünün açılması veya kapanması
+//     if (submenu.classList.contains("active")) {
+//       subcontent.style.display = "none";
+//       arrow.style.transform = "rotate(0deg)";
+//       submenu.classList.remove("active");
+//     } else {
+//       subcontent.style.display = "block";
+//       arrow.style.transform = "rotate(90deg)";
+//       submenu.classList.add("active");
+//     }
+//   });
+// });
+
+//* geliştirilecek
+
+
 // Dark Mode start
 const ball = document.querySelector(".dark-mode");
 const items = document.querySelectorAll(
